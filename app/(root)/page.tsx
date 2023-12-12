bd96faf8ece809ee73e0669dfbd65ce978cf1915
Lai Dinh Thuan (Jack) <laidinhthuan123@gmail.com>
2023-12-12 17:10:19 +0700

    implement dragable card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useRef, useState } from "react";
 import TaskCard from "../components/TaskCard";
 
 const cardcomp = [
@@ -7,13 +9,42 @@ const cardcomp = [
 ];
 
 export default function Home() {
+  const [list, setList] = useState(cardcomp);
+  const dragItem = useRef<number | undefined>();
+  const dragOverItem = useRef<number | undefined>();
+
+  const dragStart = (e: React.DragEvent<HTMLDivElement>, position: number) => {
+    dragItem.current = position;
+    console.log("position", position);
+  };
+  const dragEnter = (e: React.DragEvent<HTMLDivElement>, position: number) => {
+    dragOverItem.current = position;
+  };
+  const drop = (e: any) => {
+    if (dragItem.current !== undefined && dragOverItem.current !== undefined) {
+      const copyListItems = [...list];
+      const dragItemContent = copyListItems[dragItem.current];
+      copyListItems.splice(dragItem.current, 1);
+      copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+      dragItem.current = undefined;
+      dragOverItem.current = undefined;
+      setList(copyListItems);
+    }
+  };
   return (
-    <>
-      {cardcomp.map((item) => (
-        <div className="mb-10" key={item.title}>
+    <div className="flex space-x-5">
+      {list.map((item, index) => (
+        <div
+          onDragStart={(e) => dragStart(e, index)}
+          onDragEnter={(e) => dragEnter(e, index)}
+          onDragEnd={drop}
+          draggable
+          className="mb-10"
+          key={item.title}
+        >
           <TaskCard status={item.status} title={item.title} type={item.type} />
         </div>
       ))}
-    </>
+    </div>
   );
 }
